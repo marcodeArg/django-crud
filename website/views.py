c69b90323abc853f334cc80be6aba939e9b9e89d
mrc <marcoemorales2015@gmail.com>
2023-03-29 22:46:54 -0300

    login and register systems

--- website/views.py
+++ website/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # Login Part
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Auth
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+        else:
+            messages.error(request, 'Error')
+
+        return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Logged out!')
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You Have Successfully Registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
